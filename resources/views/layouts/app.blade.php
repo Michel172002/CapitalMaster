06a72b6e3716c817483570e917ddd4a67e9c7122
Erick Michel <michel172002@gmail.com>
2025-08-05 21:54:25 -0300

    feat: criar layout base para a aplicação

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>@yield('title', 'Capital Master') | Capital Master</title>
+
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        @livewireStyles
+
+        <!-- Styles / Scripts -->
+        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @endif
+    </head>
+    <body>
+        @yield('content')
+        
+        @livewireScripts
+    </body>
+</html>

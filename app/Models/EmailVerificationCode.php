bc970de337810e9baac06c6d82d7827037381f16
Erick Michel <michel172002@gmail.com>
2025-10-27 21:02:24 -0300

    feat: create EmailVerificationCode model and migration for email verification functionality

---
+++ app/Models/EmailVerificationCode.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class EmailVerificationCode extends Model
+{
+    protected $fillable = [
+        'email',
+        'code',
+        'expires_at',
+        'used'
+    ];
+
+    protected $casts = [
+        'expires_at' => 'datetime',
+        'used' => 'boolean'
+    ];
+
+    public function isExpired()
+    {
+        return $this->expires_at->isPast();
+    }
+
+    public function isValid()
+    {
+        return !$this->used && !$this->isExpired();
+    }
+}
